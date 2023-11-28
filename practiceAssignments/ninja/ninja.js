5b2e479a31141d317b504ac1f393bf7aa1962cf7
Frankie Lawrence <frank4law@gmail.com>
2023-11-28 12:45:47 -0500

    commit for additional assignments

---
+++ practiceAssignments/ninja/ninja.js
@@ -0,0 +1,30 @@
+class Ninja
+{
+    constructor(name, health = 90, speed = 3, strength = 3)
+    {
+        this.name = name
+        this.health = health
+        this.speed = speed
+        this.strength = strength
+    }
+
+    sayName()
+    {
+        console.log(this.name + '\n');
+    }
+    showStats()
+    {
+        console.log(`Name: ${this.name}\nStrength: ${this.strength}\nSpeed: ${this.speed}\nHealth: ${this.health}\n`);
+    }
+    drinkSake()
+    {
+        this.health += 10;
+        console.log(`Sake warms your blood and increases your vigor! Your health is now ${this.health}\n`);
+    }
+}
+
+const Haru = new Ninja("Chris", 100, 2, 99);
+Haru.sayName()
+Haru.showStats()
+Haru.drinkSake()
+
